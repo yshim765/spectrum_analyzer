a24ba805c78ba1f478dbabfdd9029646f2799281
yshim765 <yshimizu765@gmail.com>
2019-12-31 21:04:12 +0900

    modified plot func

--- spectrumanalyzer.py
+++ spectrumanalyzer.py
@@ -1,29 +1,44 @@
 import pandas as pd
 import numpy as np
-import wave
+import librosa
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 import sys, argparse
 
-def read_data(path):
-    return pd.read_csv(path, header=None)
+def read_data(path, sr):
+    data = pd.Series([np.sin(2*np.pi*x/10) + np.sin(2*np.pi*x/50) for x in range(1, 131)] +\
+                    [np.sin(2*np.pi*x/5) + np.sin(2*np.pi*x/20) for x in range(1, 131)] +\
+                    [np.sin(2*np.pi*x/10) + np.sin(2*np.pi*x/50) for x in range(1, 131)] +\
+                    [np.sin(2*np.pi*x/5) + np.sin(2*np.pi*x/20) for x in range(1, 131)])
 
-def plot_spec(i, data, sr, axes):
+    data = pd.read_csv(path, header=None)
+
+    return data.values.reshape(len(data))
+
+def plot_spec(i, data, sr, windowtype, windowsize, axes):
     """
     data: series, spectrum data
     """
+
+    window_dict = {
+        "hamming":np.hamming,
+        "hanning":np.hanning,
+        "rectangule":(lambda x: np.ones(x))
+    }
+
+    window_func = window_dict[windowtype]
+
     axes[0].cla()
     axes[1].cla()
 
-    temp = data.iloc[i:i+100]
+    temp = window_func(windowsize) * data[i:i+windowsize]
 
     fft_temp = np.fft.fft(temp)
-    freq = np.fft.fftfreq(temp.shape[0])
+    freq = np.fft.fftfreq(temp.shape[0], d=1/sr)
 
-    fft_temp = np.array([*fft_temp[int(len(fft_temp)/2):], *fft_temp[:int(len(fft_temp)/2)]])
-    freq = np.array([*freq[int(len(freq)/2):], *freq[:int(len(freq)/2)]])
-
+    fft_temp = np.array(fft_temp[0 <= freq])
+    freq = np.array(freq[0 <= freq])
 
     axes[0].plot(temp)
     axes[1].plot(freq, np.sqrt(fft_temp.real**2 + fft_temp.imag**2))
@@ -33,29 +48,31 @@ def plot_spec(i, data, sr, axes):
 
 def main(args):
     path = args.filepath
-    sr = args.sr
-    windowtype = args.windowtype
-    windowsize = args.windowsize
+    
+    if args.usesettings:
+        pass
+    else:
+        sr = args.sr
+        windowtype = args.windowtype
+        windowsize = args.windowsize
 
-    data = read_data(path)
-
-    data = pd.Series([np.sin(2*np.pi*x/10) + np.sin(2*np.pi*x/100) for x in range(1, 131)] +\
-                     [np.sin(2*np.pi*x/5) + np.sin(2*np.pi*x/20) for x in range(1, 131)] +\
-                     [np.sin(2*np.pi*x/10) + np.sin(2*np.pi*x/100) for x in range(1, 131)] +\
-                     [np.sin(2*np.pi*x/5) + np.sin(2*np.pi*x/20) for x in range(1, 131)])
+    data = read_data(path, sr)
 
     N = len(data)
 
     fig, axes = plt.subplots(2, 1, figsize=[12, 8])
-    ani = animation.FuncAnimation(fig, plot_spec, fargs = (data, sr, axes), interval=16, frames=N-100)
+    ani = animation.FuncAnimation(fig, plot_spec, fargs = (data, sr, windowtype, windowsize, axes), interval=1000/16, frames=N-windowsize)
     plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="spectrum analyzer")
     parser.add_argument("filepath", help=".wav or .csv file")
-    parser.add_argument("--sr", help="sampling rate. default=44100", type=int, default=44100)
-    parser.add_argument("--windowtype", help="window type")
-    parser.add_argument("--windowsize", help="window size", type=int, default=44100)
+    parser.add_argument("--sr", type=int, default=44100, help="sampling rate. default=44100")
+    parser.add_argument("--windowtype", default="hamming", choices=["hamming", "hanning", "rectangule"], help="window type. default=hamming. can use hamming, hanning, rectangule")
+    parser.add_argument("--windowsize", type=int, default=100, help="window size. default=100")
+    parser.add_argument("--usesettings", action='store_true', help="use saved setting file")
+    parser.add_argument("--savesettings", action='store_true', help="save settings to file")
+    
     args = parser.parse_args()
 
     main(args)
